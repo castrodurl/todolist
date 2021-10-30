f19d880ff799cae3fb2c0542404fb3a596cb2c78
Luis Castro <castrodurl@gmail.com>
2021-10-29 17:08:43 -0700

    segundo commit

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -1,35 +1,41 @@
 export class TodoList {
     constructor() {
-        this.todos = []
+        this.cargarLocalStorage()
+        /* this.todos = [] */
     }
 
     nuevoTodo(todo) {
         this.todos.push(todo)
+        this.guardarLocalStorage()
     }
 
     eliminarTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id)
+        this.todos = this.todos.filter((todo) => todo.id != id)
+        this.guardarLocalStorage()
     }
 
     toggleTodo(id) {
         for (const todo of this.todos) {
-            if(todo.id === id) {
+            if (todo.id === id) {
                 todo.completado = !todo.completado
+                this.guardarLocalStorage()
                 break
             }
         }
     }
 
     eliminarCompletados() {
-        this.todos = this.todos.filter(todo => todo.completado === true)
+        this.todos = this.todos.filter((todo) => todo.completado != true)
+        this.guardarLocalStorage()
     }
 
     guardarLocalStorage() {
-
+        localStorage.setItem("todo", JSON.stringify(this.todos))
     }
 
     cargarLocalStorage() {
-        
+        localStorage.getItem("todo")
+            ? (this.todos = JSON.parse(localStorage.getItem("todo")))
+            : (this.todos = [])
     }
-
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import './styles.css'
 import {Todo, TodoList} from './classes'
 import { crearTodoHtml } from './js/componentes'
-/* const tarea = new Todo('Aprender JavaScript') */
 export const todoList = new TodoList()
-/* todoList.nuevoTodo(tarea)
-crearTodoHtml(tarea) */
-/* 
-localStorage.setItem('mi-key', '123')
-sessionStorage.setItem('mi-key', '123')  */
-
-/* setTimeout(() => {
-    localStorage.removeItem('mi-key')
-}, 1500) */
+todoList.todos.forEach(crearTodoHtml)
